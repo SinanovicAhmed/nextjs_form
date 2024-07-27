e8b8fa0d66353f49b95a363251d68312d80b96cc
SinanovicAhmed <ahmedsinanovic20@gmail.com>
2024-07-27 11:12:50 +0200

    Added: Job display section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,25 @@
-import Image from "next/image";
+import JobListCard from "@/components/JobListCard";
+import prisma from "@/lib/prisma";
 
-export default function Home() {
-  return <main></main>;
+export default async function Home() {
+  const jobs = await prisma.job.findMany({
+    where: { approved: true },
+    orderBy: { createdAt: "desc" },
+  });
+
+  return (
+    <main>
+      <section className="max-w-3xl mx-auto space-y-3">
+        <div className="w-full text-center p-10 space-y-4">
+          <h2 className="text-3xl font-bold">Developer jobs</h2>
+          <p className="text-muted-foreground text-sm">
+            Find Your Dream Job Today
+          </p>
+        </div>
+        {jobs.map((job) => (
+          <JobListCard key={job.id} job={job} />
+        ))}
+      </section>
+    </main>
+  );
 }
---
+++ src/components/JobListCard.tsx
@@ -0,0 +1,59 @@
+import { Job } from "@prisma/client";
+import Image from "next/image";
+import { Banknote, Briefcase, Clock, MapPin } from "lucide-react";
+import { calculateDateGap, formatMoney } from "@/lib/utils";
+interface JobListCardProp {
+  job: Job;
+}
+
+const JobListCard = ({ job }: JobListCardProp) => {
+  return (
+    <article className="flex gap-5 border p-5 hover:bg-muted rounded-sm">
+      <Image
+        className="rounded-lg self-center"
+        src={job.companyLogoUrl || "/images/job_logo_default.png"}
+        alt={`${job.companyName} logo`}
+        width={90}
+        height={90}
+      />
+
+      <div className="flex-grow space-y-3">
+        <div>
+          <h2 className="font-medium">{job.title}</h2>
+          <p className="text-sm text-muted-foreground">{job.companyName}</p>
+        </div>
+        <div className="text-sm text-muted-foreground">
+          <p className="flex items-center gap-2">
+            <Briefcase size={15} className="shrink-0" />
+            {job.type}
+          </p>
+          <p className="flex items-center gap-2">
+            <MapPin size={15} className="shrink-0" />
+            {job.location || "Worldwide"}
+          </p>
+          <p className="flex items-center gap-2">
+            <Banknote size={15} className="shrink-0" />
+            {formatMoney(job.salary)}
+          </p>
+          <p className="flex items-center gap-2 sm:hidden">
+            <Briefcase size={15} className="shrink-0" />
+            {job.type}
+          </p>
+          <p className="flex items-center gap-2 sm:hidden">
+            <Clock size={15} className="shrink-0" />
+            {calculateDateGap(job.createdAt)}
+          </p>
+        </div>
+      </div>
+      <div className="hidden sm:flex flex-col justify-between items-end text-muted-foreground text-sm">
+        <p className="bg-slate-200 px-2 rounded">{job.type}</p>
+        <p className="flex items-center gap-2">
+          <Clock size={15} className="shrink-0" />
+          {calculateDateGap(job.createdAt)}
+        </p>
+      </div>
+    </article>
+  );
+};
+
+export default JobListCard;
